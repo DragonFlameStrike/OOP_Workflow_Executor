fa673f535bde59c3cc4553d078682e6963f555cb
DragonFlameStrike <91818588+DragonFlameStrike@users.noreply.github.com>
2021-10-19 22:27:28 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main() {
 
     std::map<int,std::vector<std::string>> blocks;
-    std::vector<Worker> workers = ParseWorkers("D:\\CLion 2021.2.1\\MyProjects\\Factorio\\input.txt",blocks);
+    std::vector<Worker> workers = ParseWorkers("input.txt",blocks);
 
    for (auto& c : workers){
        c.run();
