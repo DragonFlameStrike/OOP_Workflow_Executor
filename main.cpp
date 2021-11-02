5c0e2da0f2f3c121d6f2d7fb7d4168bc4e1f63e0
Dmitrii <91818588+DragonFlameStrike@users.noreply.github.com>
2021-11-02 17:30:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "Worker.h"
+#include "ParseWorkers.h"
 #include <map>
-
+#include <memory>
 
 int main() {
 
     std::map<int,std::vector<std::string>> blocks;
-    std::vector<Worker> workers = ParseWorkers("input.txt",blocks);
+    std::vector<std::shared_ptr<Worker>> workers = ParseWorkers(R"(D:\CLion 2021.2.1\MyProjects\lab_2\input.txt)",blocks);
 
    for (auto& c : workers){
-       c.run();
+
+       c->run(nullptr, nullptr);
    }
     return 0;
 }
