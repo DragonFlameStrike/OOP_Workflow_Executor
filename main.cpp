6a3c0f15dfd1e9de4953cc1620447b9686ccc268
Dmitrii <91818588+DragonFlameStrike@users.noreply.github.com>
2021-11-07 15:44:43 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,12 +7,26 @@
 int main() {
 
     std::map<int,std::vector<std::string>> blocks;
-    std::vector<std::shared_ptr<Worker>> workers = ParseWorkers(R"(D:\CLion_2021.2.1\MyProjects\lab_2\input.txt)",blocks);
+    std::vector<std::shared_ptr<Worker>> workers;
     std::vector<std::vector<std::string>> vin;
     std::vector<std::vector<std::string>> vout;
-    for (auto& c : workers){
 
-        c->run(vin, vout);
+    try{
+      workers = ParseWorkers(R"(D:\CLion_2021.2.1\MyProjects\lab_2\input.txt)",blocks);
+    }
+    catch (const std::string  ex){
+        std::cout<<ex<<std::endl;
+        return -1;
+    }
+
+    for (auto& c : workers) {
+        try {
+            c->run(vin, vout);
+        }
+        catch (const std::string  ex){
+            std::cout<<ex<<std::endl;
+            return -2;
+        }
     }
     return 0;
 }
